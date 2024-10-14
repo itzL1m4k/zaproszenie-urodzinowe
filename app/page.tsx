e2199f5d8ab10c7d09b6746ae86620e2a990499e
itzL1m4k <102859157+itzL1m4k@users.noreply.github.com>
2024-10-14 20:24:20 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Countdown from "./components/Countdown";
 import Invitation from "./components/Invitation";
 
 const HomePage = () => {
   return (
     <main>
       <Invitation />
-      <Countdown /> {/* Dodaj odliczanie */}
     </main>
   );
 };
